84bd40b9eeb581071006e25d311259f6c87c349d
TaegyuHO <dio_g@naver.com>
2016-04-24 11:11:29 +0900

    Erica's Item

---
+++ csEricaItem.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class csEricaItem : MonoBehaviour {
+
+	public bool Key;
+	public bool MemoryBook;
+	public int CloverCount;
+
+	// Use this for initialization
+	void Start () {
+		Key = false;
+		MemoryBook = false;
+		CloverCount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void AboutKey(){
+		Key = !Key;
+	}
+
+	public void AboutMemoryBook(){
+		MemoryBook = !MemoryBook;
+	}
+
+
+	public void IncreaseCloverCount(){
+		CloverCount++;
+	}
+}
